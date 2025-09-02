3aede28778523bd42da2596b251d62fd577353fb
Ikhsancnt <ikhsanputraa52@gmail.com>
2025-09-02 12:58:39 +0700

    Add package tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+    "./node_modules/flowbite/**/*.js" // Tambahkan baris ini
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin') // Tambahkan baris ini
+  ],
 }
-
